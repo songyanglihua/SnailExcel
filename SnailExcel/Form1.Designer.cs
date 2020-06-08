eac9abe81895529f6f63f339e19e876e67b46068
songyang <402009983@qq.com>
2020-06-08 16:27:00 +0800

    初始化Form显示

--- SnailExcel/Form1.Designer.cs
+++ SnailExcel/Form1.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 490);
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Excel蜗牛助手";
             this.ResumeLayout(false);
 
         }
